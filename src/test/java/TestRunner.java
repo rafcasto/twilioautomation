6619ec9d1a3309e6ec6147ae99f5e3d982338fd6
unknown <rafcasto@gmail.com>
2022-03-02 08:26:05 +1300

    adding reports

---
+++ src/test/java/TestRunner.java
@@ -0,0 +1,9 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features",plugin = {"pretty","html:target/report/index.html","json:target/report/cucumber.json"},monochrome = false)
+public class TestRunner {
+
+}
